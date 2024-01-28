155cbfba7724c148cb15d16d8990c6522ffffd14
Rytryx <imrytry@gmail.com>
2024-01-28 14:28:48 +0100

    adding login and register window

--- client/client.js
+++ client/client.js
@@ -1,11 +1,32 @@
-if (location.host.includes('localhost')) {
-  // Load livereload script if we are on localhost
-  document.write(
-    '<script src="http://' +
-      (location.host || 'localhost').split(':')[0] +
-      ':35729/livereload.js?snipver=1"></' +
-      'script>'
-  )
+function openTab(evt, tabName) {
+  const tabcontent = document.getElementsByClassName('tabcontent');
+  for (let i = 0; i < tabcontent.length; i += 1) {
+    tabcontent[i].style.display = 'none';
+  }
+
+  const tablinks = document.getElementsByClassName('tablinks');
+  for (let i = 0; i < tablinks.length; i += 1) {
+    tablinks[i].classList.remove('active');
+  }
+
+  document.getElementById(tabName).style.display = 'block';
+  evt.currentTarget.classList.add('active');
 }
 
-console.log('This is a Test')
+function handleTabClick(evt) {
+  const tabName = this.textContent.trim();
+  openTab(evt, tabName);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const tablinks = document.getElementsByClassName('tablinks');
+
+  for (let i = 0; i < tablinks.length; i += 1) {
+    tablinks[i].addEventListener('click', handleTabClick);
+  }
+
+  // Trigger click on the first tablink by default
+  if (tablinks.length > 0) {
+    tablinks[0].click();
+  }
+});
