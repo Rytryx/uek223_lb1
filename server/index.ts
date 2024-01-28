8d95f4e47f63237673513c06c443647ff27af598
Rytryx <imrytry@gmail.com>
2024-01-28 14:01:53 +0100

    configuring index.ts with new api's

--- server/index.ts
+++ server/index.ts
@@ -1,61 +1,64 @@
-import express, { Express, Request, Response } from 'express'
-import { API } from './api'
-import http from 'http'
-import { resolve, dirname } from 'path'
-import { Database } from './database'
+import express, { Express, Request, Response } from 'express';
+import http from 'http';
+import { resolve, dirname } from 'path';
+import { API } from './api';
+import { Database } from './database';
+import registrationRouter from './identity_management/register';
+import postsRouter from './content/posts';
+import commentsRouter from './content/comments';
+import likesRouter from './content/likes';
+import feedRouter from './content/feed';
+import profileRouter from './identity_management/profile';
+import loginRouter from './identity_management/login';
 
 class Backend {
   // Properties
-  private _app: Express
-  private _api: API
-  private _database: Database
-  private _env: string
-
-  // Getters
-  public get app(): Express {
-    return this._app
-  }
-
-  public get api(): API {
-    return this._api
-  }
-
-  public get database(): Database {
-    return this._database
-  }
+  public _app: Express;
+  private _api: API;
+  private _database: Database;
+  private _env: string;
 
   // Constructor
   constructor() {
-    this._app = express()
-    this._database = new Database()
-    this._api = new API(this._app)
-    this._env = process.env.NODE_ENV || 'development'
+    this._app = express();
+    this._database = new Database();
+    this._api = new API(this._app);
+    this._env = process.env.NODE_ENV || 'development';
 
-    this.setupStaticFiles()
-    this.setupRoutes()
-    this.startServer()
+    this.setupStaticFiles();
+    this.setupRoutes();
+    this.startServer();
   }
 
   // Methods
   private setupStaticFiles(): void {
-    this._app.use(express.static('client'))
+    this._app.use(express.static('client'));
   }
 
   private setupRoutes(): void {
+    // Hauptseite
     this._app.get('/', (req: Request, res: Response) => {
-      const __dirname = resolve(dirname(''))
-      res.sendFile(__dirname + '/client/index.html')
-    })
+      const __dirname = resolve(dirname(''));
+      res.sendFile(__dirname + '/client/index.html');
+    });
+
+    this._app.use('/api/register', registrationRouter);
+    this._app.use('/api/posts', postsRouter);
+    this._app.use('/api/comments', commentsRouter);
+    this._app.use('/api/likes', likesRouter);
+    this._app.use('/api/feed', feedRouter);
+    this._app.use('/api/profile', profileRouter);
+    this._app.use('/api/login', loginRouter);
   }
 
   private startServer(): void {
     if (this._env === 'production') {
-      http.createServer(this.app).listen(3000, () => {
-        console.log('Server is listening!')
-      })
+      http.createServer(this._app).listen(3000, () => {
+        console.log('Server is listening!');
+      });
     }
   }
 }
 
-const backend = new Backend()
-export const viteNodeApp = backend.app
+const backend = new Backend();
+export const viteNodeApp = backend._app;
