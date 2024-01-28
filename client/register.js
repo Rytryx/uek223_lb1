893e0d793c8eeece5d6c0324940a1bb77b85552d
Rytryx <imrytry@gmail.com>
2024-01-28 19:54:23 +0100

    adding revert fields after succesfull registration

--- client/register.js
+++ client/register.js
@@ -3,14 +3,11 @@ document.addEventListener("DOMContentLoaded", function () {
 
     registerButton.addEventListener("click", async function (event) {
         event.preventDefault();
+        console.log("Registrierungsversuch...");
 
         const username = document.getElementById("username").value;
         const email = document.getElementById("email").value;
         const password = document.getElementById("password").value;
-
-        console.log("Username:", username);
-        console.log("Email:", email);
-        console.log("Password:", password);
 
         const registrationData = {
             username: username,
@@ -19,6 +16,7 @@ document.addEventListener("DOMContentLoaded", function () {
         };
 
         try {
+            console.log("Sende Registrierungsdaten...");
             const response = await fetch("/api/register", {
                 method: "POST",
                 headers: {
@@ -28,14 +26,19 @@ document.addEventListener("DOMContentLoaded", function () {
             });
 
             if (response.ok) {
-                const data = await response.json();
-                console.log("Registrierung erfolgreich:", data);
+                console.log("Registrierung erfolgreich!");
+                alert("Registrierung erfolgreich!");
+                document.getElementById("username").value = '';
+                document.getElementById("email").value = '';
+                document.getElementById("password").value = '';
             } else {
                 const errorData = await response.json();
-                console.error("Registrierung fehlgeschlagen:", errorData);
+                console.error("Registrierung fehlgeschlagen:", errorData.message);
+                alert("Registrierung fehlgeschlagen: " + errorData.message);
             }
         } catch (error) {
             console.error("Fehler bei der Registrierung:", error);
+            alert("Fehler bei der Registrierung: " + error.message);
         }
     });
 });
--- server/user_managment/jwt.ts
+++ server/user_managment/jwt.ts
@@ -3,7 +3,12 @@ import jwt from 'jsonwebtoken';
 const secretKey = 'uek223_lb1';
 
 export function generateToken(payload: any): string {
-    return jwt.sign(payload, secretKey, { expiresIn: '1h' });
+    const convertedPayload = Object.fromEntries(
+        Object.entries(payload).map(([key, value]) => 
+            [key, (typeof value === 'bigint') ? value.toString() : value])
+    );
+
+    return jwt.sign(convertedPayload, secretKey, { expiresIn: '1h' });
 }
 
 export function verifyToken(token: string): any {
